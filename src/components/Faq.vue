9e80a4cc7d6b9254844cfa6cebd94b52b035fdeb
Sabitha <sabithajaleel84@gmail.com>
2019-07-16 09:46:51 +0400

    commit

---
+++ src/components/Faq.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="faq">
+    <div class="container">
+      <section class="section">
+        <h1 class="title">FAQ</h1>
+        <h2 class="subtitle">Fetching data from rest api</h2>
+
+        <div class="columns" v-if="faqs && faqs.length">
+          <div class="column is-one-third" v-for="faq of faqs" v-bind:key="faq">
+            <div class="card">
+              <div class="card-content">
+                <p class="title">{{faq.title}}</p>
+                <p class="subtitle">{{faq.body}}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Faq',
+  data: () => ({
+    faqs: [],
+    errors: []
+  }),
+  created() {
+    axios.get("http://jsonplaceholder.typicode.com/posts").then(response => {
+      this.faqs = response.data.slice(0,10);
+    })
+    .catch(e => {
+      this.errors.push(e);
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+@import '../common'
+
+.pd
+  padding: 2.5em 0 1.5em 0
+
+.answer
+  margin-top: 10px !important
+  color: gray
+
+.columns
+  flex-wrap: wrap
+  
+</style>
